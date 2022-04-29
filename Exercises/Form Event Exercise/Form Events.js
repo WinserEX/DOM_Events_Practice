7eae952b7b038cd391f41de3fbdd54b0d230f7d5
Your Name <you@example.com>
2022-04-29 18:21:58 -0400

    Testing events and forms

--- Exercises/Form Event Exercise/Form Events.js
+++ Exercises/Form Event Exercise/Form Events.js
@@ -1,20 +1,16 @@
-const b = document.querySelector('#bacaneria')
-const c = document.querySelector('#list')
-
-b.addEventListener('submit', function(e){
+const form = document.querySelector('form')
+const list = document.querySelector('#list')
+ 
+form.addEventListener('submit', function (e){
     e.preventDefault();
-    const productInput = b.elements.product;
-    const qInput = b.elements.qty;
-    addProduct(productInput.value, qInput.value)
-    productInput.value = '';
-    qInput.value = '';
- });
-
- const addProduct = (product, qty) => {
-    const newP = document.createElement('li');
-    const pTag = document.createElement('b');
-    pTag.append(product)
-    newP.append(pTag);
-    newP.append(`- ${qty}`)
-    c.append(newP);
-}
+    const addProduct = (prodName,q) => {
+        const newProd = document.createElement('li');
+        newProd.innerText = (`${q} ${prodName}s`);
+        list.appendChild(newProd);
+    };
+    const prodName = form.elements.product;
+    const qty = form.elements.qty;
+    addProduct(prodName.value, qty.value);
+    prodName.value ='';
+    qty.value ='';
+});
